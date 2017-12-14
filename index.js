8b642adb83d92580a5d3475477c08639b852d15a
Malte Peters <malte.peters@protofy.com>
2017-12-14 14:52:07 +0100

    add initial push api call

--- index.js
+++ index.js
@@ -1,61 +1,59 @@
-'use strict';
+"use strict";
 
-var connection = require('./lib/connection'),
-    identity = require('./lib/identity'),
-    sharingMessage = require('./lib/sharingMessage'),
-    shorturl = require('./lib/shorturl'),
-    user = require('./lib/user'),
-    sso = require('./lib/sso'),
-    analytics = require('./lib/analytics'),
-    provider = require('./lib/provider'),
-    discussion = require('./lib/discussion'),
-    Api = require('./lib/core/api'),
-    Config = require('./lib/config');
+var connection = require("./lib/connection"),
+    identity = require("./lib/identity"),
+    sharingMessage = require("./lib/sharingMessage"),
+    shorturl = require("./lib/shorturl"),
+    user = require("./lib/user"),
+    sso = require("./lib/sso"),
+    analytics = require("./lib/analytics"),
+    provider = require("./lib/provider"),
+    push = require("./lib/push"),
+    discussion = require("./lib/discussion"),
+    Api = require("./lib/core/api"),
+    Config = require("./lib/config");
 
 /**
  * Oneall sdk, agregates all functions provided by Oneall rest API
  * @param {Object} options Connection properties, and some other options {api: {endpoint:..., publickey:..., privatekey:...}, debug:false};
  */
 function Oneall(cfg) {
-
     var objects = [
-        'connection',
-        'identity',
-        'sharingMessage',
-        'shorturl',
-        'user',
-        'sso',
-        'analytics',
-        'provider',
-        'discussion'
+        "connection",
+        "identity",
+        "sharingMessage",
+        "shorturl",
+        "user",
+        "sso",
+        "analytics",
+        "provider",
+        "discussion"
     ];
 
     if (cfg === undefined) {
-        throw new Error('cfg parameter is mandatory.');
+        throw new Error("cfg parameter is mandatory.");
     }
 
     if (cfg.endpoint === undefined) {
-        throw new Error('You must set a endpoint.');
+        throw new Error("You must set a endpoint.");
     }
 
     if (cfg.publickey === undefined) {
-        throw new Error('You must set the publickey.');
+        throw new Error("You must set the publickey.");
     }
 
     if (cfg.privatekey === undefined) {
-        throw new Error('You must set the privatekey.');
+        throw new Error("You must set the privatekey.");
     }
 
     var config = new Config(cfg);
     this.api = new Api(config);
 
-
-    objects.forEach (function (obj) {
-        Object.keys(this[obj]).forEach(function (key) {
+    objects.forEach(function(obj) {
+        Object.keys(this[obj]).forEach(function(key) {
             this[obj][key] = this[obj][key].bind(this);
         }, this);
     }, this);
-
 }
 
 Oneall.prototype.connection = connection;
@@ -66,7 +64,7 @@ Oneall.prototype.identity = identity;
 Oneall.prototype.sso = sso;
 Oneall.prototype.analytics = analytics;
 Oneall.prototype.provider = provider;
+Oneall.prototype.push = push;
 Oneall.prototype.discussion = discussion;
-
 
 module.exports = Oneall;
